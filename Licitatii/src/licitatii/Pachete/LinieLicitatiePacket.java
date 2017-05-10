c454a7592a9be7e6666f0e8e2b8c78ce5cc94bde
GabiAD <gabriel.andrei96@yahoo.com>
2017-05-10 16:01:26 +0300

    Update LinieLicitatiePacket

--- Licitatii/src/licitatii/Pachete/LinieLicitatiePacket.java
+++ Licitatii/src/licitatii/Pachete/LinieLicitatiePacket.java
@@ -1,41 +1,46 @@
 package licitatii.Pachete;
 
 import javax.swing.Icon;
+import licitatii.Models.Licitation;
+import licitatii.Models.Product;
+import licitatii.Models.User;
 
 public class LinieLicitatiePacket {
 
-    Icon icon;
-    String titluStr;
-    String descriere;
-    int pretMaxim;
-    int indexLicitatie;
+    private Icon icon;  // icon-ul corespunzator imaginii din product
+    // private String titluStr; --- Product
+    // private String descriere; --- Product
+    // private int pretMaxim; --- Licitation
     
-    public LinieLicitatiePacket(Icon ic, String t, String d, int p, int in){
+    // !!!!!!!! // private int indexLicitatie; --- Licitation.id  <== Trebuie scris
+    
+    private Licitation licitatie; // licitatia
+    private Product produs; // produsul din licitatie
+    private User user; // user-ul care a licitat cel mai mult
+    
+    public LinieLicitatiePacket(Icon ic, Licitation l, User u, Product p){
         icon = ic;
-        titluStr = t;
-        descriere = d;
-        pretMaxim = p;
-        indexLicitatie = in;
+        licitatie = l;
+        user = u;
+        produs = p;
     }
 
     public Icon getIcon() {
         return icon;
     }
 
-    public String getTitluStr() {
-        return titluStr;
+    public Licitation getLicitatie() {
+        return licitatie;
     }
 
-    public String getDescriere() {
-        return descriere;
+    public Product getProdus() {
+        return produs;
     }
 
-    public int getPretMaxim() {
-        return pretMaxim;
+    public User getUser() {
+        return user;
     }
 
-    public int getIndexLicitatie() {
-        return indexLicitatie;
-    }
+    
     
 }
